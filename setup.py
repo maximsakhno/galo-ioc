ca39f18b15a17847e401404a18300bfba97f9003
Maxim Sakhno <maxim.sakhno@iqtek.ru>
2019-02-14 16:06:55 +0600

    * Update version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name="ioc",
-    version="0.0.1",
+    version="0.0.2",
     author="Maxim Sakhno",
     author_email="maxim.sakhno@iqtek.ru",
     packages=find_packages(),
