29289805a27256bcb0cab9545ea2ee7d50611600
Maxim Sakhno <maxim.sakhno@gmail.com>
2021-03-22 11:29:11 +0600

    Updated project structure.

--- setup.py
+++ setup.py
@@ -16,10 +16,8 @@ prod_requires = [
 
 test_requires = [
     *prod_requires,
-    "mypy==0.782",
     "pytest==6.0.1",
     "pytest-asyncio==0.14.0",
-    "coverage==5.2.1",
 ]
 
 
@@ -28,7 +26,8 @@ setup(
     version="0.10.0",
     author="Maxim Sakhno",
     author_email="maxim.sakhno@iqtek.ru",
-    packages=find_packages(include=["ioc", "ioc.*"]),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     install_requires=install_requires,
     tests_require=test_requires,
     extras_require={
