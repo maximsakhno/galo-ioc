c2c493c24872f92821191502749e21b57fe41067
Maxim Sakhno <maxim.sakhno@gmail.com>
2021-04-21 14:56:44 +0600

    Updated version to 0.12.0.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requires = [
 
 setup(
     name="ioc",
-    version="0.11.1",
+    version="0.12.0",
     author="Maxim Sakhno",
     author_email="maxim.sakhno@iqtek.ru",
     packages=find_packages(where="src"),
