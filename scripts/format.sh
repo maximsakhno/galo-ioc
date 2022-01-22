3b6f3a0113f60ea3d9008994442617414447ad45
Maxim Sakhno <maxim.sakhno@gmail.com>
2022-01-22 12:35:42 +0600

    Added autoflake and updated flake8 config

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -x
-set -e
 
+autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place galo_ioc examples tests
 black galo_ioc examples tests
 isort galo_ioc examples tests
